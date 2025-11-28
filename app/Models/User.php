e7cdc125bfb266f7b94a1eb76d57d6dd1a2eb2b5
inst <rostuslav9913@gmail.com>
2025-11-28 08:50:14 +0100

    User

--- app/Models/User.php
+++ app/Models/User.php
@@ -5,30 +5,37 @@ namespace App\Models;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
-{
-    use Notifiable;
-
-    // monitor DB
-    protected $connection = 'pgsql_monitor';
-
-    // Таблиця зі старого MONITOR
+//class User extends Authenticatable
+//{
+////    use Notifiable;
+//
+//    // monitor DB
+//    protected $connection = 'pgsql_monitor';
+//
+//    // Таблиця зі старого MONITOR
+//    protected $table = 'sys_users';
+//
+//    protected $primaryKey = 'id';
+//    public $timestamps = false;
+//    protected $guarded = [];
+//
+//    protected $fillable = [
+//        'email',
+//        'fullName',
+//        'role',
+//        'locationName',
+//        'locationId',
+//        'language',
+//        'password',
+//    ];
+//
+//    protected $hidden = [
+//        'password',
+//    ];
+//}
+class User extends Authenticatable {
     protected $table = 'sys_users';
-
-    protected $primaryKey = 'id';
     public $timestamps = false;
-
-    protected $fillable = [
-        'email',
-        'fullName',
-        'role',
-        'locationName',
-        'locationId',
-        'language',
-        'password',
-    ];
-
-    protected $hidden = [
-        'password',
-    ];
+    protected $guarded = [];
 }
+
--- config/auth.php
+++ config/auth.php
@@ -92,10 +92,9 @@ return [
 
     'passwords' => [
         'users' => [
-            'provider' => 'users',
-            'table' => env('AUTH_PASSWORD_RESET_TOKEN_TABLE', 'password_reset_tokens'),
-            'expire' => 60,
-            'throttle' => 60,
+            'driver' => 'database',
+            'table'  => 'sys_users',
+            'connection' => 'pgsql_monitor',
         ],
     ],
 
